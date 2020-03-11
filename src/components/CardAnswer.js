7b8e5521d4dcc00bfd426376e127cf7ab9b4b520
meloneminze <melanie@derrabus.de>
2020-03-11 21:48:18 +0100

    Add fields for question and answers

---
+++ src/components/CardAnswer.js
@@ -0,0 +1,8 @@
+import "./CardAnswer.css";
+import React from "react";
+
+function CardAnswer(props) {
+    return <div className="CardAnswer">{props.children}</div>;
+  }
+
+  export default CardAnswer;
---
+++ src/components/CardQuestion.js
@@ -0,0 +1,8 @@
+import "./CardQuestion.css";
+import React from "react";
+
+function CardQuestion(props) {
+    return <div className="CardQuestion">{props.children}</div>;
+  }
+
+  export default CardQuestion;
