951ec3423ff16cfb96b28b0b07f795ba2cd126ec
meloneminze <melanie@derrabus.de>
2020-03-12 13:00:45 +0100

    Fixed css

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -7,43 +7,73 @@ import ButtonClear from "../components/ButtonClear";
 import ButtonSave from "../components/ButtonSave";
 
 function Add() {
+  const [question, setQuestion] = React.useState("");
+  const [answerOne, setAnswerOne] = React.useState("");
+  const [answerTwo, setAnswerTwo] = React.useState("");
+  const [answerThree, setAnswerThree] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const poll = {
+      question: question,
+      answerOne: answerOne,
+      answerTwo: answerTwo,
+      answerThree: answerThree
+    };
+    alert(JSON.stringify(poll));
+  }
+
   return (
-    <div>
-      <h2>Your Question?</h2>
-      <CardQuestion>
-        <form className="add-form">
+    <div className="div">
+      <form className="add-form" onSubmit={handleSubmit}>
+        <h2>Your Question?</h2>
+        <CardQuestion>
           <input
             type="text"
             placeholder="Enter question"
             className="add-form__input add-form__input-question"
+            value={question}
+            onChange={event => {
+              setQuestion(event.target.value);
+            }}
           ></input>
-        </form>
-      </CardQuestion>
-      <h2>Possible Answers:</h2>
-      <CardAnswer>
-        <form>
+        </CardQuestion>
+        <h2>Possible Answers:</h2>
+        <CardAnswer>
           <input
             type="text"
             placeholder="Answer 1"
             className="add-form__input add-form__input-answer"
+            value={answerOne}
+            onChange={event => {
+              setAnswerOne(event.target.value);
+            }}
           ></input>
-
           <input
             type="text"
             placeholder="Answer 2"
             className="add-form__input add-form__input-answer"
+            value={answerTwo}
+            onChange={event => {
+              setAnswerTwo(event.target.value);
+            }}
           ></input>
           <input
             type="text"
             placeholder="Answer 3"
             className="add-form__input add-form__input-answer"
+            value={answerThree}
+            onChange={event => {
+              setAnswerThree(event.target.value);
+            }}
           ></input>
           <ButtonSave className="buttonSave">Save</ButtonSave>
           <ButtonClear>Clear</ButtonClear>
-        </form>
 
-        <Link to="/vote">Vote</Link>
-      </CardAnswer>
+          <Link to="/vote">Vote</Link>
+        </CardAnswer>
+      </form>
     </div>
   );
 }
