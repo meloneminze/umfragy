754737e2ee65c961d838ea015ec9ccb0b8a23f2f
meloneminze <melanie@derrabus.de>
2020-03-15 11:33:43 +0100

    Display poll by ID

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+const pollApiURL =
+  process.env.REACT_API_POLLS_API ||
+  "https://my-json-server.typicode.com/meloneminze/umfragy/polls";
 
 function Result() {
+  const { pollId } = useParams();
+  const { poll, setPoll } = React.useState(null);
+
+  React.useEffect(() => {
+    async function getPoll() {
+      const response = await fetch(`${pollApiURL}/${pollId}`);
+      const poll = await response.json();
+      setPoll(poll);
+    }
+
+    getPoll();
+  }, [pollId]);
+
   return (
     <div>
-      Result <Link to="/add">Add</Link>{" "}
+      <h2>{poll?.question}</h2>
+      <div>{poll?.answerOne}</div>
+      <div>{poll?.answerTwo}</div>
+      <div>{poll?.answerThree}</div>
     </div>
   );
 }
