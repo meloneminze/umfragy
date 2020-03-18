40a87ce0e93b0b110d4667db35a2ef0a549f793a
meloneminze <melanie@derrabus.de>
2020-03-18 11:39:54 +0100

    Change index.css to Emotion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Global, css } from "@emotion/core";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          background-color: rgb(167, 166, 166);
+          height: 100vh;
+          color: black;
+          font-family: monospace;
+        }
+      `}
+    />
+
+    <App />
+  </>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
